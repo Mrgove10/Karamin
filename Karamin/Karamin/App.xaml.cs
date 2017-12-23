d2109bfe54c64f4f8c9d6532a0291fe73c85d716
Adrien Richard <richaradrien0@gmail.com>
2017-12-24 00:57:48 +0100

    FInished 1stVer

--- Karamin/Karamin/App.xaml.cs
+++ Karamin/Karamin/App.xaml.cs
@@ -16,168 +16,200 @@ namespace Karamin
 	    /// 
         
 	    int[] ArrayPricipale = new int[4] {0,0,0,0};
-        
+
         public App()
-	    {
-	        InitializeComponent();
+        {
+            InitializeComponent();
             //Definition du text D'intro
             Label TextHaut = new Label()
-	        {
-	            Text = "Bienvenue dans Karmin, un solver de diagrame de karnaug , coder en Xamarin.Forms."
-	        };
-            //definition des boutons
-            var LabelB0 = new Label()
             {
-                Text = "00"
+                Text = "Bienvenue dans Karmin, un solver de diagrame de karnaug , coder en Xamarin.Forms.",
+                FontSize = 25,
             };
-	        var B0 = new Button()
-	        {
-	            Text = ArrayPricipale[0].ToString(),
-	            VerticalOptions = LayoutOptions.FillAndExpand,
+
+            Label TextAB = new Label()
+            {
+                Text = "AB",
+                FontSize = 25,
+            };
+
+            //definition des boutons
+            //case 0
+            Label LabelB0 = new Label()
+            {
+                Text = "00",
+                FontSize = 25,
+            };
+            Button B0 = new Button()
+            {
+                Text = ArrayPricipale[0].ToString(),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
                 BackgroundColor = Color.White,
-	        };
-            //
-	        var LabelB1 = new Label()
-	        {
-	            Text = "01"
-	        };
-            var B1 = new Button()
-	        {
-	            Text = ArrayPricipale[1].ToString(),
-	            VerticalOptions = LayoutOptions.FillAndExpand,
-	            BackgroundColor = Color.White,
+                FontSize = 35,
             };
-            //
-	        var LabelB2 = new Label()
-	        {
-	            Text = "10"
-	        };
-            var B2 = new Button()
-	        {
-	            Text = ArrayPricipale[2].ToString(),
-	            VerticalOptions = LayoutOptions.FillAndExpand,
-	            BackgroundColor = Color.White,
+            //case 1
+            Label LabelB1 = new Label()
+            {
+                Text = "01",
+                FontSize = 25,
             };
-            //
-	        var LabelB3 = new Label()
-	        {
-	            Text = "11"
-	        };
-            var B3 = new Button()
-	        {
-	            Text = ArrayPricipale[3].ToString(),
-	            VerticalOptions = LayoutOptions.FillAndExpand,
-	            BackgroundColor = Color.White,
+            Button B1 = new Button()
+            {
+                Text = ArrayPricipale[1].ToString(),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.White,
+                FontSize = 35,
+            };
+            //case 2
+            Label LabelB2 = new Label()
+            {
+                Text = "11",
+                FontSize = 25,
+            };
+            Button B2 = new Button()
+            {
+                Text = ArrayPricipale[2].ToString(),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.White,
+                FontSize = 35,
+            };
+            //case 3
+            Label LabelB3 = new Label()
+            {
+                Text = "10",
+                FontSize = 25,
+            };
+            Button B3 = new Button()
+            {
+                Text = ArrayPricipale[3].ToString(),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.White,
+                FontSize = 35,
             };
 
             //definition de la grille de bouton
             Grid GrillePrincipale = new Grid()
-	        {
-	            VerticalOptions = LayoutOptions.FillAndExpand,
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
                 RowDefinitions =
-	            {
-	                new RowDefinition {Height = GridLength.Auto},
-	            },
-	            ColumnDefinitions =
-	            {
-	                new ColumnDefinition {Width = GridLength.Auto},
-	            },
+                {
+                    new RowDefinition {Height = GridLength.Auto},
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width = GridLength.Auto},
+                },
                 BackgroundColor = Color.DarkGray,
                 Padding = 7,
-	        };
+            };
 
             //ajout des boutons a la grille
-	        GrillePrincipale.Children.Add(LabelB0, 1, 0);
+            GrillePrincipale.Children.Add(TextAB, 0, 0);
+
+            GrillePrincipale.Children.Add(LabelB0, 1, 0);
             GrillePrincipale.Children.Add(B0, 1, 1);
 
-	        GrillePrincipale.Children.Add(LabelB1, 1, 0);
+            GrillePrincipale.Children.Add(LabelB1, 2, 0);
             GrillePrincipale.Children.Add(B1, 2, 1);
 
-	        GrillePrincipale.Children.Add(LabelB2, 1, 0);
+            GrillePrincipale.Children.Add(LabelB2, 3, 0);
             GrillePrincipale.Children.Add(B2, 3, 1);
 
-	        GrillePrincipale.Children.Add(LabelB3, 1, 0);
+            GrillePrincipale.Children.Add(LabelB3, 4, 0);
             GrillePrincipale.Children.Add(B3, 4, 1);
-            
+
+            Label textBas = new Label()
+            {
+                Text = "Solution : ",
+                FontSize = 25,
+            };
+
             //definition button remise a 0
             var all0Button = new Button()
-	        {
-	            Text = "all = 0",
-	            HorizontalOptions = LayoutOptions.FillAndExpand,
-	            VerticalOptions = LayoutOptions.End,
+            {
+                Text = "all = 0",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.End,
+                FontSize = 25,
             };
 
             //definition button remise a 1
             var all1Button = new Button()
-	        {
-	            Text = "all = 1",
+            {
+                Text = "all = 1",
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.End,
-	        };
+                FontSize = 25,
+            };
+
 
             /// 
             /// Appelle de fontions.
             /// 
 	        all0Button.Clicked += foncAll0;
-	        all1Button.Clicked += foncAl1;
-	        B0.Clicked += buttonPressInGrid0;
-	        B1.Clicked += buttonPressInGrid1;
-	        B2.Clicked += buttonPressInGrid2;
-	        B3.Clicked += buttonPressInGrid3;
+            all1Button.Clicked += foncAl1;
+            B0.Clicked += buttonPressInGrid0;
+            B1.Clicked += buttonPressInGrid1;
+            B2.Clicked += buttonPressInGrid2;
+            B3.Clicked += buttonPressInGrid3;
 
             MainPage = new ContentPage
-	        {
-	            Content = new StackLayout
-	            {
-	                BackgroundColor = Color.White,
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.White,
                     Spacing = 10,
                     Padding = 1,
-	                Children =
-	                {
-	                    TextHaut,
+                    Children =
+                    {
+                        TextHaut,
                         GrillePrincipale,
-	                    all0Button,
+                        textBas,
+                        all0Button,
                         all1Button,
-	                },
+                    },
 
-	            }
-	        };
+                }
+            };
+
             /// 
             /// Definition des fonction
             /// 
 
             //fonction tu updates the label of buttons
- 	        void UpdateButtons()
-	        {
-	            B0.Text = ArrayPricipale[0].ToString();
-	            B1.Text = ArrayPricipale[1].ToString();
-	            B2.Text = ArrayPricipale[2].ToString();
-	            B3.Text = ArrayPricipale[3].ToString();
-	        }
+            void UpdateButtons()
+            {
+                B0.Text = ArrayPricipale[0].ToString();
+                B1.Text = ArrayPricipale[1].ToString();
+                B2.Text = ArrayPricipale[2].ToString();
+                B3.Text = ArrayPricipale[3].ToString();
+            }
 
-	        //fonction to put them all at 0
+            //fonction to put them all at 0
             void foncAll0(object sender, EventArgs e)
-	        {
-	            for (int i = 0; i < ArrayPricipale.Length; i++)
-	            {
-	                ArrayPricipale[i] = 0;
-	            }
+            {
+                for (int i = 0; i < ArrayPricipale.Length; i++)
+                {
+                    ArrayPricipale[i] = 0;
+                }
                 UpdateButtons();
+                calculeExpression();
             }
 
             //fonction to put them all at 1
             void foncAl1(object sender, EventArgs e)
-	        {
-	            for (int i = 0; i < ArrayPricipale.Length; i++)
-	            {
-	                ArrayPricipale[i] = 1;
-	            }
-	            UpdateButtons();
-	        }
+            {
+                for (int i = 0; i < ArrayPricipale.Length; i++)
+                {
+                    ArrayPricipale[i] = 1;
+                }
+                UpdateButtons();
+                calculeExpression();
+            }
 
             //button switch 
-	        void buttonPressInGrid0(object sender, EventArgs e)
-	        {
+            void buttonPressInGrid0(object sender, EventArgs e)
+            {
                 switch (ArrayPricipale[0])
                 {
                     case 0:
@@ -187,59 +219,131 @@ namespace Karamin
                         ArrayPricipale[0] = 0;
                         break;
                 }
-	            UpdateButtons();
+                UpdateButtons();
+                calculeExpression();
             }
-	        void buttonPressInGrid1(object sender, EventArgs e)
-	        {
-	            switch (ArrayPricipale[1])
-	            {
-	                case 0:
-	                    ArrayPricipale[1] = 1;
-	                    break;
-	                case 1:
-	                    ArrayPricipale[1] = 0;
-	                    break;
-	            }
-	            UpdateButtons();
-	        }
-	        void buttonPressInGrid2(object sender, EventArgs e)
-	        {
-	            switch (ArrayPricipale[2])
-	            {
-	                case 0:
-	                    ArrayPricipale[2] = 1;
-	                    break;
-	                case 1:
-	                    ArrayPricipale[2] = 0;
-	                    break;
-	            }
-	            UpdateButtons();
-	        }
-	        void buttonPressInGrid3(object sender, EventArgs e)
-	        {
-	            switch (ArrayPricipale[3])
-	            {
-	                case 0:
-	                    ArrayPricipale[3] = 1;
-	                    break;
-	                case 1:
-	                    ArrayPricipale[3] = 0;
-	                    break;
-	            }
-	            UpdateButtons();
-	        }
+            void buttonPressInGrid1(object sender, EventArgs e)
+            {
+                switch (ArrayPricipale[1])
+                {
+                    case 0:
+                        ArrayPricipale[1] = 1;
+                        break;
+                    case 1:
+                        ArrayPricipale[1] = 0;
+                        break;
+                }
+                UpdateButtons();
+                calculeExpression();
+            }
+            void buttonPressInGrid2(object sender, EventArgs e)
+            {
+                switch (ArrayPricipale[2])
+                {
+                    case 0:
+                        ArrayPricipale[2] = 1;
+                        break;
+                    case 1:
+                        ArrayPricipale[2] = 0;
+                        break;
+                }
+                UpdateButtons();
+                calculeExpression();
+            }
+            void buttonPressInGrid3(object sender, EventArgs e)
+            {
+                switch (ArrayPricipale[3])
+                {
+                    case 0:
+                        ArrayPricipale[3] = 1;
+                        break;
+                    case 1:
+                        ArrayPricipale[3] = 0;
+                        break;
+                }
+                UpdateButtons();
+                calculeExpression();
+            }
 
-	        void calculeExpression()
-	        {
-	            
-	        }
+            void calculeExpression()
+            {
 
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : 0";
+                }
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : 1";
+                }
+                //1
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : /A /B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : /A B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : A B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : A /B";
+                }
+
+                //2
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : /A";
+                }
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : /A /B + A B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : /A B + A /B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : A ";
+                }
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : /B ";
+                }
+                //3
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 0)
+                {
+                    textBas.Text = "Solution : /A + B";
+                }
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 0 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : /A + /B";
+                }
+                if (ArrayPricipale[0] == 0 && ArrayPricipale[1] == 1 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : B + A";
+                }
+                if (ArrayPricipale[0] == 1 && ArrayPricipale[1] == 0 && ArrayPricipale[2] == 1 && ArrayPricipale[3] == 1)
+                {
+                    textBas.Text = "Solution : /B + A ";
+                }
+
+            }
         }
 
 	    protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+
+        }
 
 		protected override void OnSleep ()
 		{
